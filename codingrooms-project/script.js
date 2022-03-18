2e74e0cad86450db7fc1bb345425c494cc5e9026
Jeshuahsps <98600568+Jeshuahsps@users.noreply.github.com>
2022-03-18 09:45:45 -0400

    Delete codingrooms-project directory

--- codingrooms-project/script.js
+++
@@ -1,4 +0,0 @@
-/* This is where we put our global variables */
-var questions = parseInt(document.getElementById("questions").value);
-var min = parseInt(document.getElementById("min").value);
-var max = parseInt(document.getElementById("max").value);
