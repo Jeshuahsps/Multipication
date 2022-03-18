a6a8b9ad3b3066135c8d49a3964a9b6f30e76f07
Jeshuahsps <98600568+Jeshuahsps@users.noreply.github.com>
2022-03-18 09:37:11 -0400

    Add files via upload

---
+++ codingrooms-project/script.js
@@ -0,0 +1,4 @@
+/* This is where we put our global variables */
+var questions = parseInt(document.getElementById("questions").value);
+var min = parseInt(document.getElementById("min").value);
+var max = parseInt(document.getElementById("max").value);
